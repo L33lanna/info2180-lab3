d87f2159047274b16d6602d5e17404f49117ed63
L33lanna <haydenleanna@yahoo.com>
2020-10-13 23:08:30 -0500

    Part 1- Layout

---
+++ tic-tac-toe.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// Task 1: Layout the board 
+
+window.addEventListener("load", (event)=> {
+    work();
+});
+
+function work(){
+
+    document.querySelectorAll("board > div").classList.add("square");
+    
+    let start = document.getElementsByClassName("btn")[0];
+    
+    start.setAttribute("click","newGame();");
+
+    start.addEventListener('mouseover', function(x) {
+        x.target.classList.add('hover');
+    });
+
+    start.addEventListener('mouseout', function (b) {
+        b.target.classList.remove('hover');
+    });
+}
+
